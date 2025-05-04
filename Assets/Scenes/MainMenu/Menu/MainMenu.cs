880679d24e2bdb5ac753e25d923d9dfdfca46b2f
Christian Toney <christian.toney@beastslash.com>
2025-05-04 00:46:56 -0400

    add main menu

---
+++ Assets/Scenes/MainMenu/Menu/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenu : MonoBehaviour
+{
+  
+  public Player player;
+  public ProgressReport progressReport;
+
+  void Start()
+  {
+
+    if (TryGetComponent<UIDocument>(out var uiDocument))
+    {
+      // Get the root visual element
+      VisualElement root = uiDocument.rootVisualElement;
+
+      // Find the button by name and add a click event listener
+      Button startButton = root.Q<Button>("PlayButton");
+      startButton.RegisterCallback<ClickEvent>(ev => {
+
+        progressReport.enabled = true;
+
+      });
+
+      Button exitButton = root.Q<Button>("ExitButton");
+      exitButton.RegisterCallback<ClickEvent>(ev => {
+
+        Application.Quit();
+
+      });
+    }
+
+  }
+}
